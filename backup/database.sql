7208220ef9c607e8bc19668875bebea7cb7bd44d
Ubuntu <ubuntu@ip-172-31-42-100.ec2.internal>
2024-01-17 12:01:09 +0000

    More Backup

--- backup/database.sql
+++ backup/database.sql
@@ -16,21 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `activations`
---
-
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `activations` (
-  `id` varchar(255) NOT NULL,
-  `code` varchar(255) DEFAULT NULL,
-  `valid` tinyint(1) DEFAULT '1',
-  `created_at` timestamp(6) NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Dumping data for table `activations`
 --
 
@@ -38,24 +23,6 @@ LOCK TABLES `activations` WRITE;
 /*!40000 ALTER TABLE `activations` DISABLE KEYS */;
 /*!40000 ALTER TABLE `activations` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `courses`
---
-
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `courses` (
-  `id` varchar(255) NOT NULL,
-  `subject` varchar(255) DEFAULT NULL,
-  `grade` varchar(255) DEFAULT NULL,
-  `banner` varchar(255) DEFAULT NULL,
-  `review` int DEFAULT '0',
-  `rate` float(8,2) DEFAULT '0.00',
-  `created_at` timestamp(6) NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `courses`
@@ -67,20 +34,6 @@ LOCK TABLES `courses` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `grades`
---
-
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `grades` (
-  `id` varchar(255) NOT NULL,
-  `level` varchar(255) DEFAULT NULL,
-  `created_at` timestamp(6) NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Dumping data for table `grades`
 --
 
@@ -88,25 +41,6 @@ LOCK TABLES `grades` WRITE;
 /*!40000 ALTER TABLE `grades` DISABLE KEYS */;
 /*!40000 ALTER TABLE `grades` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `instructors`
---
-
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `instructors` (
-  `id` varchar(255) NOT NULL,
-  `firstName` varchar(255) DEFAULT NULL,
-  `lastName` varchar(255) DEFAULT NULL,
-  `profilePic` varchar(255) DEFAULT NULL,
-  `review` int DEFAULT '0',
-  `gender` enum('male','female') DEFAULT NULL,
-  `rate` float(8,2) DEFAULT '0.00',
-  `created_at` timestamp(6) NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `instructors`
@@ -118,29 +52,6 @@ LOCK TABLES `instructors` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `lessons`
---
-
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `lessons` (
-  `id` varchar(255) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `course` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `instructorName` varchar(255) DEFAULT NULL,
-  `instructorPic` varchar(255) DEFAULT NULL,
-  `video` varchar(255) DEFAULT NULL,
-  `doc` varchar(255) DEFAULT NULL,
-  `duration` float(8,2) DEFAULT NULL,
-  `created_at` timestamp(6) NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`id`),
-  KEY `lessons_course_foreign` (`course`),
-  CONSTRAINT `lessons_course_foreign` FOREIGN KEY (`course`) REFERENCES `courses` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Dumping data for table `lessons`
 --
 
@@ -148,21 +59,6 @@ LOCK TABLES `lessons` WRITE;
 /*!40000 ALTER TABLE `lessons` DISABLE KEYS */;
 /*!40000 ALTER TABLE `lessons` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `subjects`
---
-
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `subjects` (
-  `id` varchar(255) NOT NULL,
-  `title` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `created_at` timestamp(6) NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `subjects`
@@ -174,30 +70,6 @@ LOCK TABLES `subjects` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `tests`
---
-
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tests` (
-  `id` varchar(255) NOT NULL,
-  `question` varchar(255) DEFAULT NULL,
-  `optionA` varchar(255) DEFAULT NULL,
-  `optionB` varchar(255) DEFAULT NULL,
-  `optionC` varchar(255) DEFAULT NULL,
-  `optionD` varchar(255) DEFAULT NULL,
-  `answer` varchar(255) DEFAULT NULL,
-  `type` varchar(255) DEFAULT NULL,
-  `lesson` varchar(255) DEFAULT NULL,
-  `duration` float(8,2) DEFAULT NULL,
-  `created_at` timestamp(6) NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`id`),
-  KEY `tests_lesson_foreign` (`lesson`),
-  CONSTRAINT `tests_lesson_foreign` FOREIGN KEY (`lesson`) REFERENCES `lessons` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Dumping data for table `tests`
 --
 
@@ -205,24 +77,6 @@ LOCK TABLES `tests` WRITE;
 /*!40000 ALTER TABLE `tests` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tests` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `usercourses`
---
-
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `usercourses` (
-  `id` varchar(255) NOT NULL,
-  `user` varchar(255) DEFAULT NULL,
-  `course` varchar(255) DEFAULT NULL,
-  `lessons` varchar(255) DEFAULT NULL,
-  `status` enum('inprogress','completed') DEFAULT NULL,
-  `progress` int DEFAULT NULL,
-  `created_at` timestamp(6) NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `usercourses`
@@ -234,33 +88,6 @@ LOCK TABLES `usercourses` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `users`
---
-
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `id` varchar(255) NOT NULL,
-  `userId` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `code` varchar(255) DEFAULT NULL,
-  `verify` tinyint(1) DEFAULT '0',
-  `firstName` varchar(255) DEFAULT NULL,
-  `lastName` varchar(255) DEFAULT NULL,
-  `location` varchar(255) DEFAULT NULL,
-  `isAdmin` tinyint(1) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `gender` enum('male','female') DEFAULT NULL,
-  `dob` date DEFAULT NULL,
-  `grade` varchar(255) DEFAULT NULL,
-  `profilePic` varchar(255) DEFAULT NULL,
-  `created_at` timestamp(6) NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `users_userid_unique` (`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Dumping data for table `users`
 --
 
@@ -269,24 +96,6 @@ LOCK TABLES `users` WRITE;
 INSERT  IGNORE INTO `users` VALUES ('9dfd1d28-dcab-4161-aa96-d999bb21fe6d','455447','$2b$10$5aDMBQ5VhJBkqrxLs9HXLujm.mf0ih0cNZo/i2jKUdAnt4yktfkCa',NULL,0,NULL,NULL,NULL,1,'ajedamilola2005@gmail.com',NULL,NULL,NULL,NULL,'2024-01-17 10:21:30.954017');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `usertests`
---
-
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `usertests` (
-  `id` varchar(255) NOT NULL,
-  `user` varchar(255) DEFAULT NULL,
-  `lesson` varchar(255) DEFAULT NULL,
-  `tests` varchar(255) DEFAULT NULL,
-  `score` float(8,2) DEFAULT NULL,
-  `status` enum('inprogress','completed') DEFAULT NULL,
-  `created_at` timestamp(6) NULL DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `usertests`
@@ -306,4 +115,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-01-17 11:56:18
+-- Dump completed on 2024-01-17 12:01:09
