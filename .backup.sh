9f145ff488040f3e9468c533f5c00ae00867432d
Ubuntu <ubuntu@ip-172-31-42-100.ec2.internal>
2024-01-17 12:02:43 +0000

    More Backup

--- .backup.sh
+++ .backup.sh
@@ -1,4 +1,4 @@
-mysqldump -u root -p123456 --insert-ignore --skip-add-drop-table --no-create-info elearning > backup/database.sql
+mysqldump -u root -p123456 --no-create-info --skip-add-drop-table --skip-triggers --insert-ignore  elearning > backup/database.sql
 git add .
 git commit -m "More Backup"
 git push
