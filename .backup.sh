71f0fb77e681af6ccee1812e3c0d93c792f2fbf8
Aje Damilola <ajedamilola2005@gmail.com>
2024-01-17 12:55:56 +0100

    slsls

--- .backup.sh
+++ .backup.sh
@@ -1,4 +1,4 @@
-mysqldump -u root -p123456 --insert-ignore elearning > backup/database.sql
+mysqldump -u root -p123456 --insert-ignore --skip-add-drop-table elearning > backup/database.sql
 git add .
 git commit -m "More Backup"
 git push
